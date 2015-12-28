0648994afde026c51cedc62f85bdd42a884c72d7
hschoenburg <hschoenburg@gmail.com>
2015-12-28 07:12:41 -0800

    basic player functionality ready

--- src/player1.js
+++ src/player1.js
@@ -1,10 +1,17 @@
 function Player() {
+  this.moves = ['R','P','S'];
 
   this.throwMove = function() {
     return 'R';
   };
 
-  this.recordOpponentMove = function(opponents_hand) {
+  this.recordOpponentMove = function(opponents_move) {
+    if(this.moves.indexOf(opponents_move) > 0) {
+      this.gameplay.push(opponents_move);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   this.gameplay = [];
